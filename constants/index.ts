0f6cdab5bb15210380e005dcd3a69ac25f912d98
anhnt56 <tunavn1994@gmail.com>
2023-11-09 21:21:18 +0700

    add new

---
+++ constants/index.ts
@@ -0,0 +1,128 @@
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/discord.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/facebook.svg",
+    },
+    {
+      name: "Instagram",
+      src: "/instagram.svg",
+    },
+  ];
+  
+  export const Development_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Python",
+      Image: "/python.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "TypeScript",
+      Image: "/ts.png",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "Java",
+      Image: "/java.png",
+      width: 85,
+      height: 85,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/mysql.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  export const Operation_skill = [  
+    {
+      skill_name: "Docker",
+      Image: "/docker.webp",
+      width: 72,
+      height: 72,
+    },
+    {
+      skill_name: "Kubernetes",
+      Image: "/k8s.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Terraform",
+      Image: "/terraform.png",
+      width: 63,
+      height: 63,
+    },
+    {
+      skill_name: "AWS",
+      Image: "/aws.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "GCP",
+      Image: "/gcp.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Monitoring = [
+    {
+      skill_name: "Grafana",
+      Image: "/grafana.png",
+      width: 60,
+      height: 60,
+    },
+    {
+      skill_name: "Git",
+      Image: "/git.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Prometheus",
+      Image: "/prometheus.png",
+      width: 60,
+      height: 60,
+    },
+    
+  ];
+  
+  export const Other_skill = [
+    {
+      skill_name: "Bash",
+      Image: "/bash.png",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  
+  
+  
+  

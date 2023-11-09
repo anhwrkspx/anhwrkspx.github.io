beaf54087a65aba3f813290e72d9e85c6f49d033
anhnt56 <tunavn1994@gmail.com>
2023-11-09 21:31:50 +0700

    fix html error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/anhwrkspx.github.io",
+    output:"export",
+    distDir:"dist",
+    images:{
+        unoptimized:true,
+    },
+}
 
 module.exports = nextConfig
